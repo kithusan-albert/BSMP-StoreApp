a6b19b484e25053f5b1a142d42920ac17fcca94e
Kithusan Albert <139725319+kithusan-albert@users.noreply.github.com>
2024-04-30 23:10:47 +1200

    update

--- bsmp_storeapp/__init__.py
+++ bsmp_storeapp/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, redirect, url_for
 
+# Import blueprints from different modules
+# Each blueprint handles different sections of the application
 from .Auth import auth as auth_blueprint
 from .Admin import admin as admin_blueprint
 from .Customer import customer as customer_blueprint
@@ -7,15 +9,14 @@ from .Client import client as client_blueprint
 from .Cart import cart as cart_blueprint
 from .Store import store as store_blueprint
 
-
-
 def create_app():
+    # Initialize a Flask application
     app = Flask(__name__)
     
-    # Set up app secret key
+    # Set secret key
     app.secret_key = "Our group secret key is 345"
 
-    # Registering blueprintsw
+    # Register blueprints with specific URL prefixes, all routes in the module will have this as prefix
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
     app.register_blueprint(admin_blueprint, url_prefix='/admin')
     app.register_blueprint(customer_blueprint, url_prefix='/customer')
@@ -23,8 +24,10 @@ def create_app():
     app.register_blueprint(cart_blueprint, url_prefix='/cart')
     app.register_blueprint(store_blueprint, url_prefix='/store')
 
+    # Define the root route of the application which redirects to the store home page
     @app.route('/')
     def index():
         return redirect(url_for('store.home'))
     
+    # Return the configured Flask app
     return app
--- run.py
+++ run.py
@@ -1,8 +1,10 @@
+# Import the create_app function from the bsmp_storeapp 
 from bsmp_storeapp import create_app
 
+# Create an instance of the Flask application using the create_app function
 app = create_app()
 
 if __name__ == '__main__':
+    # Run the application with debugging enabled
     app.run(debug=True)
-
 
